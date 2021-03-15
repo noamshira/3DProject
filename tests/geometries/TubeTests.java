ce79b11fd2037a500f9d5db5827afc573265573a
Noam Shira <noamisrael10@gmail.com>
2021-03-15 21:17:41 +0200

    add test

--- tests/geometries/TubeTests.java
+++ tests/geometries/TubeTests.java
@@ -1,6 +1,9 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +11,9 @@ class TubeTests {
 
     @Test
     void getNormal() {
+        Ray ray = new Ray(new Point3D(0, 1, 0), new Vector(0, 1, 0));
+        Tube tb = new Tube(ray, 2);
+
+        assertEquals(tb.getNormal(new Point3D(0, 0, 0)), new Vector(0, -1, 0));
     }
 }
